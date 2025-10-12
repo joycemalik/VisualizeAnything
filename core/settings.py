95d0365fd6d2254285a0fa02bdd9d34ce708a0b8
shark4real <saif74hassan@gmail.com>
2025-10-12 16:40:13 +0530

    Update ALLOWED_HOSTS

--- core/settings.py
+++ core/settings.py
@@ -38,7 +38,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure--a-q1h7#o!(4@(_0yzny%eh^-t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True').lower() == 'true'
 
-ALLOWED_HOSTS = ['datanaut.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ["datanaut.onrender.com","datanaut.up.railway.app","datanaut-ai.onrender.com", "localhost", "127.0.0.1"]
 
 
 
